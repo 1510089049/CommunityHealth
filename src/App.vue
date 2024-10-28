163775f7ac20f2423e1403bf08a3d17d1a7ed652
root <1510089049@QQ.com>
2024-10-28 14:48:40 +0800

    项目创建，初始提交

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HealthComp></HealthComp>
+  </div>
+</template>
+<script>
+
+import HealthComp from './components/firstComponet.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HealthComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/firstComponet.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>this is firs page</h1>
+</template>
+
+<script>
+export default {
+  name: 'HealthComp'
+
+}
+</script>
+
+<style></style>
